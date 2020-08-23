ad23f4d6d30bd7aaa8cf2c052ca70c3aa58c4244
Marcus Pöhls <marcus.poehls@gmail.com>
2020-08-23 14:13:43 +0200

    move to jest

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+'use strict'
+
+module.exports = {
+  collectCoverage: true,
+  coverageReporters: ['text', 'html'],
+  testMatch: ['**/test/**/*.[jt]s?(x)']
+}
--- test/index.js
+++ test/index.js
@@ -3,232 +3,227 @@
 const RequestIp = require('../dist')
 const { getClientIp } = RequestIp
 
-const Lab = require('@hapi/lab')
-const { expect } = require('@hapi/code')
-
-const { describe, it } = exports.lab = Lab.script()
-
 describe('Request IP: ', () => {
   it('exports a function', async () => {
-    expect(getClientIp).to.be.a.function()
+    expect(getClientIp).toBeInstanceOf(Function)
   })
 
   it('request headers is undefined', async () => {
-    expect(RequestIp.getClientIp()).to.be.undefined()
-    expect(RequestIp.getClientIp(null)).to.be.undefined()
+    expect(RequestIp.getClientIp()).toBeUndefined()
+    expect(RequestIp.getClientIp(null)).toBeUndefined()
 
-    expect(RequestIp.getClientIp({})).to.be.undefined()
-    expect(RequestIp.getClientIp(123)).to.be.undefined()
-    expect(RequestIp.getClientIp('request')).to.be.undefined()
+    expect(RequestIp.getClientIp({})).toBeUndefined()
+    expect(RequestIp.getClientIp(123)).toBeUndefined()
+    expect(RequestIp.getClientIp('request')).toBeUndefined()
   })
 
   it('handles null values', async () => {
     expect(
       RequestIp.getClientIp({ info: { remoteAddress: undefined } })
-    ).to.be.undefined()
+    ).toBeUndefined()
 
     expect(
       RequestIp.getClientIp({ info: { remoteAddress: null } })
-    ).to.be.undefined()
+    ).toBeUndefined()
 
     expect(
       RequestIp.getClientIp({ headers: { remoteAddress: null } })
-    ).to.be.undefined()
+    ).toBeUndefined()
   })
 
   it('x-client-ip', async () => {
     expect(
       RequestIp.getClientIp({ headers: { 'x-client-ip': '8.8.8.8' } })
-    ).to.equal('8.8.8.8')
+    ).toEqual('8.8.8.8')
 
     expect(
       RequestIp.getClientIp({ headers: { 'x-client-ip': 'not-an-ip' } })
-    ).to.be.undefined()
+    ).toBeUndefined()
   })
 
   it('fastly-client-ip', async () => {
     expect(
       RequestIp.getClientIp({ headers: { 'fastly-client-ip': '8.8.8.8' } })
-    ).to.equal('8.8.8.8')
+    ).toEqual('8.8.8.8')
   })
 
   it('cf-connecting-ip', () => {
     expect(
       RequestIp.getClientIp({ headers: { 'cf-connecting-ip': '8.8.8.8' } })
-    ).to.equal('8.8.8.8')
+    ).toEqual('8.8.8.8')
   })
 
   it('true-client-ip', () => {
     expect(
       RequestIp.getClientIp({ headers: { 'true-client-ip': '8.8.8.8' } })
-    ).to.equal('8.8.8.8')
+    ).toEqual('8.8.8.8')
   })
 
   it('x-real-ip', () => {
     expect(
       RequestIp.getClientIp({ headers: { 'x-real-ip': '8.8.8.8' } })
-    ).to.equal('8.8.8.8')
+    ).toEqual('8.8.8.8')
   })
 
   it('x-cluster-client-ip', () => {
     expect(
       RequestIp.getClientIp({ headers: { 'x-cluster-client-ip': '8.8.8.8' } })
-    ).to.equal('8.8.8.8')
+    ).toEqual('8.8.8.8')
   })
 
   it('x-forwarded-for', () => {
     expect(
       RequestIp.getClientIp({ headers: { 'x-forwarded-for': null } })
-    ).to.be.undefined()
+    ).toBeUndefined()
     expect(
       RequestIp.getClientIp({ headers: { 'x-forwarded-for': undefined } })
-    ).to.be.undefined()
+    ).toBeUndefined()
 
     expect(
       RequestIp.getClientIp({ headers: { 'x-forwarded-for': '8.8.8.8' } })
-    ).to.equal('8.8.8.8')
+    ).toEqual('8.8.8.8')
 
     expect(
       RequestIp.getClientIp({ headers: { 'x-forwarded-for': '8.8.8.8, 4.4.4.4' } })
-    ).to.equal('8.8.8.8')
+    ).toEqual('8.8.8.8')
 
     expect(
       RequestIp.getClientIp({ headers: { 'x-forwarded-for': '8.8.8.8, 4.4.4.4, 1.1.1.1' } })
-    ).to.equal('8.8.8.8')
+    ).toEqual('8.8.8.8')
   })
 
   it('x-forwarded-for with masked (unknown) IPs', () => {
     expect(
       RequestIp.getClientIp({ headers: { 'x-forwarded-for': 'unknown, [redacted], 8.8.8.8, 4.4.4.4' } })
-    ).to.equal('8.8.8.8')
+    ).toEqual('8.8.8.8')
   })
 
   it('x-forwarded-for with port', () => {
     expect(
       RequestIp.getClientIp({ headers: { 'x-forwarded-for': 'unknown, 8.8.8.8:443, 4.4.4.4:443' } })
-    ).to.equal('8.8.8.8')
+    ).toEqual('8.8.8.8')
   })
 
   it('forwarded-for', () => {
     expect(
       RequestIp.getClientIp({ headers: { 'forwarded-for': '8.8.8.8' } })
-    ).to.equal('8.8.8.8')
+    ).toEqual('8.8.8.8')
 
     expect(
       RequestIp.getClientIp({ headers: { 'forwarded-for': '8.8.8.8, 4.4.4.4' } })
-    ).to.equal('8.8.8.8')
+    ).toEqual('8.8.8.8')
 
     expect(
       RequestIp.getClientIp({ headers: { 'forwarded-for': 'unknown, unknown, 8.8.8.8, 4.4.4.4' } })
-    ).to.equal('8.8.8.8')
+    ).toEqual('8.8.8.8')
   })
 
   it('x-forwarded', () => {
     expect(
       RequestIp.getClientIp({ headers: { 'x-forwarded': '8.8.8.8' } })
-    ).to.equal('8.8.8.8')
+    ).toEqual('8.8.8.8')
 
     expect(
       RequestIp.getClientIp({ headers: { 'x-forwarded': '8.8.8.8, 4.4.4.4' } })
-    ).to.equal('8.8.8.8')
+    ).toEqual('8.8.8.8')
 
     expect(
       RequestIp.getClientIp({ headers: { 'x-forwarded': 'unknown, unknown, 8.8.8.8, 4.4.4.4' } })
-    ).to.equal('8.8.8.8')
+    ).toEqual('8.8.8.8')
   })
 
   it('forwarded', () => {
     expect(
       RequestIp.getClientIp({ headers: { forwarded: '8.8.8.8' } })
-    ).to.equal('8.8.8.8')
+    ).toEqual('8.8.8.8')
 
     expect(
       RequestIp.getClientIp({ headers: { forwarded: '8.8.8.8, 4.4.4.4' } })
-    ).to.equal('8.8.8.8')
+    ).toEqual('8.8.8.8')
 
     expect(
       RequestIp.getClientIp({ headers: { forwarded: 'unknown, unknown, 8.8.8.8, 4.4.4.4' } })
-    ).to.equal('8.8.8.8')
+    ).toEqual('8.8.8.8')
   })
 
   it('request.connection', () => {
     expect(
-      RequestIp.getClientIp({ connection: { remoteAddress: '8.8.8.8' } })).to.equal('8.8.8.8')
+      RequestIp.getClientIp({ connection: { remoteAddress: '8.8.8.8' } })).toEqual('8.8.8.8')
     expect(
       RequestIp.getClientIp({ connection: { } })
-    ).to.be.undefined()
+    ).toBeUndefined()
 
     expect(
-      RequestIp.getClientIp({ connection: { remoteAddress: 'not-an-ip-address' } })).to.be.undefined()
+      RequestIp.getClientIp({ connection: { remoteAddress: 'not-an-ip-address' } })).toBeUndefined()
   })
 
   it('request.connection.socket', () => {
     expect(
-      RequestIp.getClientIp({ connection: { socket: { remoteAddress: '8.8.8.8' } } })).to.equal('8.8.8.8')
+      RequestIp.getClientIp({ connection: { socket: { remoteAddress: '8.8.8.8' } } })).toEqual('8.8.8.8')
     expect(
-      RequestIp.getClientIp({ connection: { socket: { } } })).to.be.undefined()
+      RequestIp.getClientIp({ connection: { socket: { } } })).toBeUndefined()
     expect(
-      RequestIp.getClientIp({ connection: { socket: { remoteAddress: 'invalid-ip' } } })).to.be.undefined()
+      RequestIp.getClientIp({ connection: { socket: { remoteAddress: 'invalid-ip' } } })).toBeUndefined()
   })
 
   it('request.socket', () => {
     expect(
-      RequestIp.getClientIp({ socket: { remoteAddress: '8.8.8.8' } })).to.equal('8.8.8.8')
+      RequestIp.getClientIp({ socket: { remoteAddress: '8.8.8.8' } })).toEqual('8.8.8.8')
     expect(
       RequestIp.getClientIp({ socket: { remoteAddress: 'invalid-ip' } })
-    ).to.be.undefined()
+    ).toBeUndefined()
 
     expect(
       RequestIp.getClientIp({ socket: { } })
-    ).to.be.undefined()
+    ).toBeUndefined()
   })
 
   it('request.info', () => {
     expect(
-      RequestIp.getClientIp({ info: { remoteAddress: '8.8.8.8' } })).to.equal('8.8.8.8')
+      RequestIp.getClientIp({ info: { remoteAddress: '8.8.8.8' } })).toEqual('8.8.8.8')
     expect(
       RequestIp.getClientIp({ info: { remoteAddress: 'invalid-ip' } })
-    ).to.be.undefined()
+    ).toBeUndefined()
 
     expect(
       RequestIp.getClientIp({ info: { } })
-    ).to.be.undefined()
+    ).toBeUndefined()
   })
 
   it('request.requestContext', () => {
     expect(
       RequestIp.getClientIp({ requestContext: { identity: { sourceIp: '8.8.8.8' } } })
-    ).to.equal('8.8.8.8')
+    ).toEqual('8.8.8.8')
 
     expect(
       RequestIp.getClientIp({ requestContext: { identity: { sourceIp: 'invalid-ip' } } })
-    ).to.be.undefined()
+    ).toBeUndefined()
 
     expect(
       RequestIp.getClientIp({ requestContext: { } })
-    ).to.be.undefined()
+    ).toBeUndefined()
 
     expect(
       RequestIp.getClientIp({ requestContext: { identity: {} } })
-    ).to.be.undefined()
+    ).toBeUndefined()
   })
 
   it('request.raw', () => {
     expect(
       RequestIp.getClientIp({ raw: { info: { remoteAddress: '8.8.8.8' } } })
-    ).to.equal('8.8.8.8')
+    ).toEqual('8.8.8.8')
 
     expect(
       RequestIp.getClientIp({ raw: { info: { remoteAddress: 'invalid-ip' } } })
-    ).to.be.undefined()
+    ).toBeUndefined()
 
     expect(
       RequestIp.getClientIp({ raw: { info: {} } })
-    ).to.be.undefined()
+    ).toBeUndefined()
 
     expect(
       RequestIp.getClientIp({ raw: { } })
-    ).to.be.undefined()
+    ).toBeUndefined()
   })
 
   it('supports IPv6 addresses', () => {
@@ -236,18 +231,18 @@ describe('Request IP: ', () => {
       RequestIp.getClientIp({
         connection: { remoteAddress: '2001:0db8:85a3:0000:0000:8a2e:0370:7334' }
       })
-    ).to.equal('2001:0db8:85a3:0000:0000:8a2e:0370:7334')
+    ).toEqual('2001:0db8:85a3:0000:0000:8a2e:0370:7334')
 
     expect(
       RequestIp.getClientIp({
         headers: { 'x-forwarded-for': '2001:0db8:85a3:0000:0000:8a2e:0370:7334' }
       })
-    ).to.equal('2001:0db8:85a3:0000:0000:8a2e:0370:7334')
+    ).toEqual('2001:0db8:85a3:0000:0000:8a2e:0370:7334')
   })
 
   it('supports shortened IPv6 addresses', () => {
     expect(
       RequestIp.getClientIp({ connection: { remoteAddress: '2001:db8::2:1' } })
-    ).to.equal('2001:db8::2:1')
+    ).toEqual('2001:db8::2:1')
   })
 })
